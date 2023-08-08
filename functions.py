c7460814101baad7bbdb7651b1c772b1f617d235
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-08 04:28:16 -0500

    implemented weather data for data plot and display data plot

--- functions.py
+++ functions.py
@@ -1,17 +1,9 @@
 import requests
 
-
 API_KEY = "50a62aff2e129eb5d365687a1c7256c7"
 
-
-'''
-def get_data(days):
-    dates = ["2022-35-10", "2022-26-09", "2022-23-08"]
-    temperature = [10,15,9]
-    temperatures = [days * i for i in temperature]
-    return dates, temperatures
-'''
-def get_data(place, forecast_days, kind):
+# below is a function for pulling data from the api and filtering it based on the place
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -19,13 +11,6 @@ def get_data(place, forecast_days, kind):
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]   # filter data by forecast days
 
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]    # filter temp data via temp dict key
-    elif kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]  # filter sky data (via weather dict key)
-
     return filtered_data
 
 
-if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind='Temperature'))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import plotly.express as px
-import functions
+from functions import get_data
 
-# web app features
+# web app features ( title, text input, slider, select box)
 st.title("Weather Dashboard")
 place = st.text_input("Place:")
 days = st.slider("Forecast Days", min_value=1,
@@ -20,8 +20,23 @@ elif days == 1 and option == "Temperature":
 else:
     st.subheader(f"Below is the {option} for the next {days} days in {place}")
 
+if place:
+    # get weather data
+    filtered_data = get_data(place, days)
 
-# dynamic data plot
-d, t = functions.get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y":"Temperature (Celsius)"})
-st.plotly_chart(figure)
+    # dynamic data plot
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]  # filter temp data via temp dict key
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature (Celsius)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear":"images/clear.png",
+                  "Clouds":"images/cloud.png",
+                  "Rain":"images/rain.png",
+                  "Snow":"images/snow.png",}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]  # filter sky data via weather dict key
+        image_path = [images[condition] for condition in sky_conditions]    # access image value from key
+        st.image(image_path, width=110)
+
